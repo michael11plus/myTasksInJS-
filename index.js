aa3636197bee71142d36f5687f8fd4e1b965adb9
Michael A <azilinon.michael@gmail.com>
2024-08-22 16:43:42 +0200

    New functions with buttons

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// find the longest word in a string
 
+// find the longest word in a string
 function getLongestWord() {
     const sentence = prompt('Enter a sentence of multiple words');
     let splitSentence = sentence.split(" ");
@@ -14,6 +14,7 @@ function getLongestWord() {
     console.log(`The longest word of the sentence \n\n ${sentence} \nis\n ${longestWord}`);
 }
 
+// reverse a string
 function reverseString() {
     const yourString = prompt('Enter your string, even of multiple words');
     let newString = [];
@@ -30,7 +31,7 @@ function reverseString() {
     alert(newString);
 }
 
-
+// reverse a sentence by words
 function reverseSentence() {
     const yourSentence = prompt('Type in your sentence to reverse it by words.');
     let axeSentence = yourSentence.split(" ");
@@ -46,4 +47,66 @@ function reverseSentence() {
 
     newSentence = newSentence.join(' ')
     alert(newSentence);
+}
+
+// Write a function that compares two numbers
+function getBiggerNumber() {
+    const firstNumber = parseInt(prompt('Enter the first number you want to compare'));
+    const secondNumber = parseInt(prompt('Enter the second number you want to compare'));
+    
+    if (firstNumber < secondNumber) { 
+        alert(`Between ${firstNumber} and ${secondNumber} --- ${secondNumber} is bigger.\nThe second number is bigger by ${secondNumber - firstNumber}`);
+    } else if (firstNumber > secondNumber) {
+        alert(`Between ${firstNumber} and ${secondNumber} --- ${firstNumber} is bigger.\nThe first number is bigger by ${firstNumber - secondNumber}`);
+    } else if (firstNumber === secondNumber) {
+        alert(`Between ${firstNumber} and ${secondNumber} --- none is bigger.\nThey are the same.`);
+    }
+}
+
+// Write a function that calculates factorial of the number
+function getFactorial() {
+    const numberToFactorial = parseInt(prompt('Enter number to get factorial'));
+    let finalNumber = numberToFactorial;
+    let i = 1;
+    
+    while (i < numberToFactorial) {
+        finalNumber = finalNumber * i;
+        i++;
+    }
+
+    alert(`Your number ${numberToFactorial} in factorial is: ${finalNumber}`);
+}
+
+// Write a function that takes three digits and combines them into one number
+function combineDigits() {
+    const firstDigit = parseInt(prompt('Enter first digit of the three-digit number'));
+    const secondDigit = parseInt(prompt('Enter second digit of the three-digit number'));
+    const thirdDigit = parseInt(prompt('Enter third digit of the three-digit number'));
+    let digitArray = [];
+
+    digitArray[0] = firstDigit;
+    digitArray[1] = secondDigit;
+    digitArray[2] = thirdDigit;
+
+    alert(`You entered the digits ${firstDigit}, ${secondDigit}, ${thirdDigit} and it results in ${digitArray.join('')}`);
+}
+
+
+// Function to calculate area 
+function calculateArea(wid, len) {
+    if (arguments.length === 0) {
+        // alert('Your parameters cannot be calculated.');
+        return;
+    } 
+
+    if (arguments.length === 1 && typeof wid === 'number') {
+        return wid * wid;
+    }
+
+    if (arguments.length === 2 && typeof wid === 'number' && typeof len === 'number') {
+        return wid * len;
+    }
+
+    // alert('Your parameters cannot be calculated.');
+    return;
 }
